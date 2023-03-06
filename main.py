2bb511935637be451f937cfe8db492344e1024ac
Oleksandra Landau <landausa23@gmail.com>
2023-03-06 16:59:16 +0100

    I've done!!!

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello World")
+def palindrome(str):
+    rev = ''.join(reversed(str))
+    if (str == rev):
+        return True
+    return False
+str = "wow"
+answer = palindrome(str)
+print(palindrome(str))
+
+
+
